60ca4e29da7b09349c044c33ab0dabe523a00092
insane-22 <bhumikabachchan11315@gmail.com>
2025-08-27 15:51:52 +0530

    murf api & twilio working togetger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import twilio from "twilio";
+import axios from "axios";
 
 dotenv.config();
 const app = express();
@@ -17,18 +18,42 @@ app.post("/whatsapp", async (req, res) => {
   const incomingMsg = req.body.Body;
   const from = req.body.From;
   const to = req.body.To;
-  const audioUrl =
-    "https://murf.ai/user-upload/one-day-temp/19ce52a3-eb35-4538-877d-8aa4fe07d981.mp3?response-cache-control=max-age%3D604801&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250827T000000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=259200&X-Amz-Credential=AKIA27M5532DYKBCJICE%2F20250827%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Signature=2946f653535d457aec4315ac96855394a13a3fc00a493a9f2806ba992c1819b3";
 
   console.log("Received:", incomingMsg, "from:", from, "to:", to);
+  try {
+    const murfRes = await axios.post(
+      "https://api.murf.ai/v1/speech/generate",
+      {
+        text: incomingMsg,
+        format: "mp3",
+        voiceId: "en-US-natalie",
+      },
+      {
+        headers: {
+          "api-key": process.env.MURF_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  await client.messages.create({
-    from: to, 
-    to: from, 
-    mediaUrl: [audioUrl],
-  });
+    const murfJson = murfRes.data;
+    const audioUrl = murfJson.audioFile;
+    await client.messages.create({
+      from: to,
+      to: from,
+      mediaUrl: [audioUrl],
+    });
+    res.send("OK");
+  } catch (error) {
+    console.error("Error:", error);
+    await client.messages.create({
+      from: to,
+      to: from,
+      body: "Sorry, voice reply failed. Here's text instead: " + incomingText,
+    });
 
-  res.send("OK");
+    res.send("Fallback OK");
+  }
 });
 
 app.listen(PORT, () => {
